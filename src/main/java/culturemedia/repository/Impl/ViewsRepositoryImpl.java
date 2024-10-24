9f522c180d1f0b2e3455ce92f2df0da8daf6296d
Salvador Areiza <areiza0329@gmail.com>
2024-10-23 22:59:52 -0500

    5to commit - agregando implementación de las interfaces

---
+++ src/main/java/culturemedia/repository/Impl/ViewsRepositoryImpl.java
@@ -0,0 +1,20 @@
+package culturemedia.repository.Impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import culturemedia.model.View;
+import culturemedia.repository.ViewsRepository;
+import org.apache.logging.log4j.core.Logger;
+
+
+public class ViewsRepositoryImpl implements ViewsRepository {
+
+
+    @Override
+    public void view(View view) {
+
+    }
+}
