a0ee91ea1dba258e03739c12578bf52dab5ca7a3
Salvador Areiza <areiza0329@gmail.com>
2024-10-25 15:37:39 -0500

    8vo commit - agregando casos de prueba e implementaciones

--- src/main/java/culturemedia/repository/Impl/ViewsRepositoryImpl.java
+++ src/main/java/culturemedia/repository/Impl/ViewsRepositoryImpl.java
@@ -1,22 +1,71 @@
-package culturoteca.repository.impl;
+package culturemedia.repository.Impl;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import culturoteca.model.View;
-import culturoteca.repository.ViewsRepository;
+import culturemedia.exception.VideoNotFoundException;
+import culturemedia.model.Video;
+import culturemedia.model.View;
+import culturemedia.repository.ViewsRepository;
 
 public class ViewsRepositoryImpl implements ViewsRepository {
 
+	private final List<Video> videos;
 	private final List<View> views;
 
 	public ViewsRepositoryImpl() {
+		this.videos = new ArrayList<>();
 		this.views = new ArrayList<>();
 	}
 
 	@Override
-	public View save(View view) {
-		this.views.add( view );
-		return view;
+	public void view(View view) {
+		this.views.add(view);
+		//return view;
 	}
+
+	@Override
+	public View save(View view) {
+		this.views.add(view);
+		return view;  // Cambiado para devolver el objeto guardado
+	}
+
+	@Override
+	public Video save(Video video) {
+		this.videos.add(video);
+		return video;  // Cambiado para devolver el objeto guardado
+	}
+
+	@Override
+	public List<Video> findAll() throws VideoNotFoundException {
+		if (videos.isEmpty()) {
+			throw new VideoNotFoundException("No se encontraron videos en el repositorio.");
+		}
+		return videos;
+	}
+
+	@Override
+	public List<Video> find(String title) {
+		List<Video> findVideos = new ArrayList<>();
+		for (Video video : videos) {
+			if (title.equals(video.title())) {
+				findVideos.add(video);
+			}
+		}
+		return findVideos;
+	}
+
+	@Override
+	public List<Video> find(Double fromDuration, Double toDuration) {
+		List<Video> findVideos = new ArrayList<>();
+		for (Video video : videos) {
+			if (video.duration() >= fromDuration && video.duration() <= toDuration) {
+				findVideos.add(video);
+			}
+		}
+		return findVideos;
+	}
+
+
+
 }
--- src/main/java/culturemedia/repository/Impl/ViewsRepositoryImpl1.java
+++ src/main/java/culturemedia/repository/Impl/ViewsRepositoryImpl1.java
@@ -1,5 +1,6 @@
 package culturemedia.repository.Impl;
 
+import culturemedia.model.Video;
 import culturemedia.model.View;
 import culturemedia.repository.ViewsRepository;
 
@@ -10,5 +11,16 @@ public class ViewsRepositoryImpl1 implements ViewsRepository {
     @Override
     public void view(View view) {
 
+    }
+
+    @Override
+    public View save(View view) {
+
+        return view;
+    }
+
+    @Override
+    public Video save(Video video) {
+        return null;
     }
 }
