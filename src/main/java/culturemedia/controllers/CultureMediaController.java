25ba7533599fe13d6c51b8a0308633557917f765
SalvadorAreiza <areiza0329@gmail.com>
2024-10-30 20:49:30 -0500

    10mo commit - Modificaciones al codigo anterior según estructura de la clase controller, agregando la el controlador de CultureMediaService -
    Modifications to the previous code according to the structure of the controller class, adding the CultureMediaService controller

---
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -0,0 +1,27 @@
+package culturemedia.controllers;
+
+import java.util.*;
+
+import culturemedia.exception.VideoNotFoundException;
+import culturemedia.model.Video;
+import culturemedia.service.CultureMediaService;
+
+
+public class CultureMediaController {
+
+	private final CultureMediaService cultureMediaService;
+
+
+	public CultureMediaController(CultureMediaService cultureMediaService) {
+		this.cultureMediaService = cultureMediaService;
+	}
+
+
+	public List<Video> find_allVideos() throws VideoNotFoundException {
+		List<Video> videos = null;
+		videos = cultureMediaService.findAll();
+		return videos;
+	}
+
+
+}
